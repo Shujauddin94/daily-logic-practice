578e207ee75a5d8cf98a61f4a27fac40517671d1
Shujauddin94 <shujauddin9432@gmail.com>
2026-01-02 22:20:05 +0500

    reverse a string

--- Reverse a String/script.js
+++ Reverse a String/script.js
@@ -1,1 +1,9 @@
-//
+// Write a function that takes a string as input and returns the string reversed.
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+console.log(reverseString("hello")); // Output: "olleh"
+console.log(reverseString("JavaScript")); // Output: "tpircSavaJ"
+// split('') converts the string into an array of characters
+// reverse() reverses the array
+// join('') combines the array back into a string
