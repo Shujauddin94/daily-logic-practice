6371664a5e803b421662a7819924917cfcb1d26d
Shujauddin94 <shujauddin9432@gmail.com>
2026-01-03 23:21:04 +0500

    palindrome logic

---
+++ Palindrome check/script.js
@@ -0,0 +1,17 @@
+// Write a function to check if a given string is a palindrome
+function isPalindrome(str) {
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
+
+
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello"));   // false
+
+// If you want to that ignores spaces, punctuation, and case, use this instead
+
+
+function isPalindrome2(str) {
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleaned === cleaned.split('').reverse().join('');
+}
