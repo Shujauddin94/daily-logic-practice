8666d086da393e25f062097d7a7f579b36041ff3
Shujauddin94 <shujauddin9432@gmail.com>
2026-01-09 23:04:07 +0500

    count vowels

---
+++ Count Vowels/script.js
@@ -0,0 +1,12 @@
+// Write a function that counts the number of vowels in a given string. 
+function countVowels(str) {
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowels("Hello World")); // Output: 3
