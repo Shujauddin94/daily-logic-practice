29b9a50d5843b3c6a876daa672743ae9a43867cb
Shujauddin94 <shujauddin9432@gmail.com>
2026-01-11 23:09:56 +0500

    missing number

---
+++ Missing Number/script.js
@@ -0,0 +1,7 @@
+// Write a function to find the missing number in an array of integers from 1 to n
+function findMissingNumber(arr, n) {
+    const expectedSum = (n * (n + 1)) / 2;
+    const actualSum = arr.reduce((acc, num) => acc + num, 0);
+    return expectedSum - actualSum;
+}
+console.log(findMissingNumber([1, 2, 4, 5, 6], 6)); // Output: 3
