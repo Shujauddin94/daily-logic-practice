17d3f69d6a069f52d0cdb6195cf7fa007fe71d6f
Shujauddin94 <shujauddin9432@gmail.com>
2026-01-13 23:06:58 +0500

    uppercase of words (first character)

---
+++ Case a Senence/script.js
@@ -0,0 +1,9 @@
+// Write a function that converts the first letter of each word in a sentence to uppercase.
+function capitalizeWords(sentence) {
+    let words = sentence.split(" "); // Split sentence into words
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1); // Capitalize first letter
+    }
+    return words.join(" "); // Join words back into a sentence
+}
+console.log(capitalizeWords("hello world from javascript")); // Output: "Hello World From Javascript"
