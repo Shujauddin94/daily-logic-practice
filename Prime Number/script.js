90b199138b78cc5c31614549a66b25cef42ceea7
Shujauddin94 <shujauddin9432@gmail.com>
2026-01-17 23:01:18 +0500

    prime number

---
+++ Prime Number/script.js
@@ -0,0 +1,14 @@
+// Write a function to check if a given number is prime
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) {
+            return false;
+        }
+    }
+    return true;
+}   
+console.log(isPrime(11)); // true
+console.log(isPrime(15)); // false
