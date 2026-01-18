63a2340e5edd613af26e351b8644344716ca4382
Shujauddin94 <shujauddin9432@gmail.com>
2026-01-18 17:04:19 +0500

    added another function

--- Merge Sorted Array/script.js
+++ Merge Sorted Array/script.js
@@ -18,3 +18,35 @@ function mergeSortedArrays(arr1, arr2) {
     return merged;
 }
 console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
+
+// Another way to do this
+function mergeSortedArrays2(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  // Compare elements from both arrays
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+
+  // Add remaining elements (if any)
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+console.log(mergeSortedArrays2([1, 2, 4], [1, 3, 4])); // Output: [1, 1, 2, 3, 4, 4]
