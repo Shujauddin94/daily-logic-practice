081acabf75e460604ea30268b86961dcd43ce513
Shujauddin94 <shujauddin9432@gmail.com>
2026-01-20 18:07:12 +0500

    calculate power

---
+++ Calculate Power/script.js
@@ -0,0 +1,5 @@
+// Write a function to calculate the power of a number.
+function calculatePower(base, exponent) {
+    return Math.pow(base, exponent); // Using Math.pow to calculate power
+}
+console.log(calculatePower(2, 3)); // Output: 8
