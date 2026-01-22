418614c5655872671dca7e4dea83e1b9d6335ab8
Shujauddin94 <shujauddin9432@gmail.com>
2026-01-22 23:10:19 +0500

    added another function

--- Intersection of Arrays/script.js
+++ Intersection of Arrays/script.js
@@ -4,3 +4,9 @@ function intersection(arr1, arr2) {
     return arr1.filter(item => set2.has(item)); // Return elements present in both arrays
 }
 console.log(intersection([1, 2, 3], [2, 3, 4])); // Output: [2, 3]
+
+// Another way using filter and includes
+function intersectionUsingIncludes(arr1, arr2) {
+    return arr1.filter(item => arr2.includes(item)); // Return elements present in both arrays
+}
+console.log(intersectionUsingIncludes([1, 2, 3], [2, 3, 4])); // Output: [2, 3]
