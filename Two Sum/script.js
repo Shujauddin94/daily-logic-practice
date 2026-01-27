2f242c2005f7db20ce38f7ad7babf8f7363959af
Shujauddin94 <shujauddin9432@gmail.com>
2026-01-27 22:58:35 +0500

    added another function

--- Two Sum/script.js
+++ Two Sum/script.js
@@ -4,10 +4,22 @@ function twoSum(numbers, target) {
     for (let i = 0; i < numbers.length; i++) {
         const complement = target - numbers[i];
         if (numMap.has(complement)) {
-            return [numMap.get(complement), i];
+            return [numMap.get(complement), i]; 
         }
         numMap.set(numbers[i], i);
     }
     return null;
 }
 console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
+
+// Easier method (less efficient)
+function twoSumSlow(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+console.log(twoSumSlow([2, 7, 11, 15], 9)); // Output: [0, 1]
