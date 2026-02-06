86be23da71c6e0935e4b8d16bd598f123cbf27cc
Shujauddin94 <shujauddin9432@gmail.com>
2026-02-06 20:58:15 +0500

    added comment

--- Median of Two Sorted Array/script.js
+++ Median of Two Sorted Array/script.js
@@ -47,7 +47,7 @@ function findMedianSortedArrays(nums1, nums2) {
     const partitionX = Math.floor((low + high) / 2);
     const partitionY = Math.floor((x + y + 1) / 2) - partitionX; // Ensure left half has one more element if total length is odd
 
-    const maxLeftX = partitionX === 0 ? -Infinity : nums1[partitionX - 1];
+    const maxLeftX = partitionX === 0 ? -Infinity : nums1[partitionX - 1]; // If partitionX is 0, it means there are no elements on the left side of nums1, so we use -Infinity to ensure it doesn't affect the max calculation.
     const minRightX = partitionX === x ? Infinity : nums1[partitionX];
 
     const maxLeftY = partitionY === 0 ? -Infinity : nums2[partitionY - 1];
