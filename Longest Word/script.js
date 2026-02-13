d454fe82e6577b19c3bf6ec45e9033e5de22b8d8
Shujauddin94 <shujauddin9432@gmail.com>
2026-02-13 23:12:31 +0500

    added comment

--- Longest Word/script.js
+++ Longest Word/script.js
@@ -1,6 +1,6 @@
 // Write a function that takes a sentence (string) and finds longest word.
 function findLongestWord(sentence) {
-    const words = sentence.split(' ');
+    const words = sentence.split(' '); // Split the sentence into an array of words
     let longestWord = '';          // Initialize with an empty string
     for (let word of words) {
         if (word.length > longestWord.length) {        // Compare lengths            
