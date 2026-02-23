f9683e8939c1b47b0d570bb42b6a9804c01620cc
Shujauddin94 <shujauddin9432@gmail.com>
2026-02-23 21:38:38 +0500

    added comment

--- Fibonacci iteratively/script.js
+++ Fibonacci iteratively/script.js
@@ -19,7 +19,7 @@ function fibonacciArr(n) {
   const fib = [0, 1];
 
   for (let i = 2; i <= n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
+    fib[i] = fib[i - 1] + fib[i - 2]; // Calculate the i-th Fibonacci number and store it in the array
   }
 
   return fib[n];
