dce1a3aab3b384cf38a9b8e9f81f3cfbb71e5dc7
Shujauddin94 <shujauddin9432@gmail.com>
2026-02-23 21:38:17 +0500

    added comment

--- Fibonacci iteratively/script.js
+++ Fibonacci iteratively/script.js
@@ -1,7 +1,7 @@
 // Write a funcion to generate the n-th Fibonacci number iteratively.
 function fibonacci(n) {
     if (n <= 0) return 0;
-    if (n === 1) return 1;
+    if (n === 1) return 1; // Base cases
     let a = 0, b = 1, temp;
     for (let i = 2; i <= n; i++) {
         temp = a + b;
