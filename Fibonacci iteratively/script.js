cf51ee91448cd831f23d63e13b34f3ff3a7f4551
Shujauddin94 <shujauddin9432@gmail.com>
2026-02-25 21:55:19 +0500

    added comment

--- Fibonacci iteratively/script.js
+++ Fibonacci iteratively/script.js
@@ -8,7 +8,7 @@ function fibonacci(n) {
         a = b; // Update a to the previous Fibonacci number
         b = temp; // Update a and b to the next two Fibonacci numbers
     }
-    return b;
+    return b; // Return the n-th Fibonacci number
 }
 console.log(fibonacci(6)); // Output: 8
 console.log(fibonacci(10)); // Output: 55
