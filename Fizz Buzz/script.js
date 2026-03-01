6bfc8e46eb00241a34ebf7118aa268185ce764cd
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-01 22:06:42 +0500

    added comment

--- Fizz Buzz/script.js
+++ Fizz Buzz/script.js
@@ -1,7 +1,7 @@
 // Write a function that returns a string, such as, For multiples of three, return "Fizz" instead of the number and for the multiples of five, return "Buzz". For numbers which are multiples of both three and five, return "FizzBuzz".
 function fizzBuzz(n) {
     if (n % 3 === 0 && n % 5 === 0) {
-        return "FizzBuzz";
+        return "FizzBuzz"; // Return "FizzBuzz" if n is a multiple of both 3 and 5
     } else if (n % 3 === 0) {
         return "Fizz";
     } else if (n % 5 === 0) {
