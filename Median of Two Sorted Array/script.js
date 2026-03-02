5de364c9287d1d147164c36b8f42b4754db4e674
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-02 22:55:08 +0500

    added comment

--- Median of Two Sorted Array/script.js
+++ Median of Two Sorted Array/script.js
@@ -21,7 +21,7 @@ function findMedianSortedArrays(nums1, nums2) {
     }
     let len = merged.length;
     if (len % 2 === 0) {
-        return (merged[len / 2 - 1] + merged[len / 2]) / 2;
+        return (merged[len / 2 - 1] + merged[len / 2]) / 2; // If the total length of the merged array is even, the median is the average of the two middle elements. We access these elements using their indices, which are len / 2 - 1 and len / 2.
     } else {
         return merged[Math.floor(len / 2)];
     }
