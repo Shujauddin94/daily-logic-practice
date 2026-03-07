fd9adcfd5a6addfd9595623a4f4f3311d1a4b12f
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-07 22:36:32 +0500

    added comment

--- Occurances of Each Char/script.js
+++ Occurances of Each Char/script.js
@@ -14,6 +14,6 @@ function countCharacterOccurrencesMap(str) {
     for (let char of str) {
         charCountMap.set(char, (charCountMap.get(char) || 0) + 1); // Update the count for each character in the Map
     }
-    return charCountMap;
+    return charCountMap;// Return the Map containing character counts
 }
 console.log(countCharacterOccurrencesMap("hello world")); //Map(8) { 'h' => 1, 'e' => 1, 'l' => 3, 'o' => 2, ' ' => 1, 'w' => 1, 'r' => 1, 'd' => 1 }
