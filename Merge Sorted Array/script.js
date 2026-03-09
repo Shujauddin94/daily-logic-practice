259aaf09b226bb78864783208f8f491ae2d10669
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-09 22:57:20 +0500

    added comment

--- Merge Sorted Array/script.js
+++ Merge Sorted Array/script.js
@@ -15,7 +15,7 @@ function mergeSortedArrays(arr1, arr2) {
 
     // Append any remaining elements from either array
     merged = merged.concat(arr1.slice(i)).concat(arr2.slice(j));
-    return merged;
+    return merged; // Return the merged sorted array
 }
 console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
 
