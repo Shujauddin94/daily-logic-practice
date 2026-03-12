0cd94ad134cf1dcbcfa049a5787b22865dc736aa
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-12 22:22:08 +0500

    added comment

--- Reverse an Integer/script.js
+++ Reverse an Integer/script.js
@@ -2,7 +2,7 @@
 function reverseInteger(num) {
     const isNegative = num < 0;
     const reversedNum = parseInt(Math.abs(num).toString().split('').reverse().join(''), 10); // Convert back to integer
-    return isNegative ? -reversedNum : reversedNum;
+    return isNegative ? -reversedNum : reversedNum; // Preserve the sign of the original number
 }
 console.log(reverseInteger(12345)); // Output: 54321
 
