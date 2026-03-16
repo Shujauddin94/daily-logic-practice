5048044beb2e6022ff0aedc67f6f63fcd808f195
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-16 23:22:31 +0500

    added js

--- Rotate Array/script.js
+++ Rotate Array/script.js
@@ -12,7 +12,7 @@ function rotateRight(arr, k) {
   const n = arr.length; // Get the length of the array
   k = k % n; // Handle cases where k is greater than the array length
 
-  for (let i = 0; i < k; i++) { 
+  for (let i = 0; i < k; i++) {  // Loop k times to rotate the array
     arr.unshift(arr.pop());// Remove the last element and add it to the front of the array
   }
 
