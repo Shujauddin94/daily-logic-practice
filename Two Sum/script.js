bc17ae1f129df4af1eb597800d0fea8f2c583701
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-21 23:29:11 +0500

    added js

--- Two Sum/script.js
+++ Two Sum/script.js
@@ -17,7 +17,7 @@ function twoSumSlow(nums, target) {
   for (let i = 0; i < nums.length; i++) { // Loop through each number
     for (let j = i + 1; j < nums.length; j++) { // Loop through the numbers after the current number
       if (nums[i] + nums[j] === target) {
-        return [i, j];
+        return [i, j]; // Return indices of the two numbers that add up to the target
       }
     }
   }
