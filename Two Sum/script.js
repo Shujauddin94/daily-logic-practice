4237581db39d9b91f6cef8d2c504db9fd33292e5
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-21 23:30:52 +0500

    added js

--- Two Sum/script.js
+++ Two Sum/script.js
@@ -16,7 +16,7 @@ console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
 function twoSumSlow(nums, target) {
   for (let i = 0; i < nums.length; i++) { // Loop through each number
     for (let j = i + 1; j < nums.length; j++) { // Loop through the numbers after the current number
-      if (nums[i] + nums[j] === target) {
+      if (nums[i] + nums[j] === target) { // Check if the sum of the two numbers equals the target
         return [i, j]; // Return indices of the two numbers that add up to the target
       }
     }
