5375407086ea912821acefe6d523a1bb802a5f09
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-24 13:58:12 +0500

    added js

--- Calculate GCD/script.js
+++ Calculate GCD/script.js
@@ -1,6 +1,6 @@
 // Write a function to calculate the GCD of two numbers.
 function gcd(a, b) {
-    if (b === 0) {
+    if (b === 0) { // Base case: if the second number is 0, the GCD is the first number
         return a; // If the second number is 0, return the first number as the GCD
     }
     return gcd(b, a % b); // Recursive call with the second number and the remainder of the division of the first number by the second number
