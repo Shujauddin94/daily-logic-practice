2ccd8693259ed01e41cc8453ece6585ae068cc77
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-26 16:11:11 +0500

    added js

--- Case a Senence/script.js
+++ Case a Senence/script.js
@@ -11,9 +11,9 @@ console.log(capitalizeWords("hello world from javascript")); // Output: "Hello W
 // easier way better than for loop
 
 function capitalizeWords2(sentence) {
-  return sentence
+  return sentence // Take the input sentence
     .split(" ") // Split sentence into words
     .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize first letter using map
-    .join(" "); // Join words back into a sentence
+    .join(" "); // Join words back into a sentence 
 }
 console.log(capitalizeWords2("hello world from javascript")); // Output: "Hello World From Javascript"..
