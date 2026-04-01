82e926a3eb757cd43ce88d249f86e183eeee9770
Shujauddin94 <shujauddin9432@gmail.com>
2026-04-01 18:29:29 +0500

    added js

--- Count Words/script.js
+++ Count Words/script.js
@@ -2,7 +2,7 @@
 function countWords(sentence) {
     // Split the sentence by spaces and filter out any empty strings
     const words = sentence.trim().split(/\s+/).filter(word => word.length > 0);
-    return words.length;
+    return words.length; // Return the number of words
 }
 console.log(countWords("Hello, how are you today?")); // Output: 6
 
