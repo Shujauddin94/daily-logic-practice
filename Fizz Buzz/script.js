381ddbf560f0b246c155c8b85fe2e951062ccd2f
Shujauddin94 <shujauddin9432@gmail.com>
2026-04-12 22:26:45 +0500

    added js

--- Fizz Buzz/script.js
+++ Fizz Buzz/script.js
@@ -20,6 +20,6 @@ function fizzBuzz2(n) {
   if (n % 3 === 0) result += "Fizz";// Append "Fizz" to the result if n is a multiple of 3
   if (n % 5 === 0) result += "Buzz"; // Append "Buzz" to the result if n is a multiple of 5
 
-  return result || n.toString(); 
+  return result || n.toString(); // Return the result if it's not empty, otherwise return n as a string
 }
 console.log(fizzBuzz2(10)); // Output: Buzz
