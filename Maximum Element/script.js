af497df64cc37f71c2f885d67100a3794eeb228d
Shujauddin94 <shujauddin9432@gmail.com>
2026-04-17 22:10:35 +0500

    added js

--- Maximum Element/script.js
+++ Maximum Element/script.js
@@ -19,4 +19,4 @@ function findMax2(arr) {
   return Math.max(...arr); // Spread the array elements as individual arguments to Math.max
 }
 
-console.log(findMax2([3, 7, 2, 9, 5])); // 9
+console.log(findMax2([3, 7, 2, 9, 5])); 
