897f608584cce864023c5b17c941d395473eb41b
Shujauddin94 <shujauddin9432@gmail.com>
2026-04-26 23:02:22 +0500

    added js

--- Median of Two Sorted Array/script.js
+++ Median of Two Sorted Array/script.js
@@ -70,7 +70,7 @@ function findMedianSortedArrays(nums1, nums2) {
     }
     // Move right
     else {
-      low = partitionX + 1;
+      low = partitionX + 1; // If the maximum of the left half of nums1 is greater than the minimum of the right half of nums2, it means we need to move the partition in nums1 to the left, so we update high to partitionX - 1.
     }
   }
 }
