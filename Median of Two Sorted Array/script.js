a89c99cfdea54fdef94b21e513bfd06454331e10
Shujauddin94 <shujauddin9432@gmail.com>
2026-05-01 23:18:16 +0500

    added js

--- Median of Two Sorted Array/script.js
+++ Median of Two Sorted Array/script.js
@@ -40,7 +40,7 @@ function findMedianSortedArrays(nums1, nums2) {
   const x = nums1.length;
   const y = nums2.length;
 
-  let low = 0; // We initialize low to 0 and high to the length of nums1, which represents the range of possible partition indices for nums1. We will use binary search to find the correct partition index for nums1.
+  let low = 0; 
   let high = x; // We initialize low to 0 and high to the length of nums1, which represents the range of possible partition indices for nums1. We will use binary search to find the correct partition index for nums1.
 
   while (low <= high) {
