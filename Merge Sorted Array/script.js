36e8e42b8a9f1e8e2c405493b7737eaf2cfb40be
Shujauddin94 <shujauddin9432@gmail.com>
2026-05-12 22:57:59 +0500

    added js

--- Merge Sorted Array/script.js
+++ Merge Sorted Array/script.js
@@ -5,7 +5,7 @@ function mergeSortedArrays(arr1, arr2) {
 
     while (i < arr1.length && j < arr2.length) {
         if (arr1[i] < arr2[j]) {
-            merged.push(arr1[i]); // Add the smaller element to the merged array
+            merged.push(arr1[i]); 
             i++; // Move the pointer for arr1
         } else {
             merged.push(arr2[j]); // Add the smaller element to the merged array
