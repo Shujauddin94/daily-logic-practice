3872a943591fbb6002638496fe984d4b1e4c5369
Shujauddin94 <shujauddin9432@gmail.com>
2026-05-18 19:49:24 +0500

    added js

--- Remove Duplicates/script.js
+++ Remove Duplicates/script.js
@@ -2,4 +2,4 @@
 function removeDuplicates(arr) {
     return [...new Set(arr)]; // Using Set to remove duplicates
 }
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); 
