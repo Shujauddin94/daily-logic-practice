028cfa13a49a729956cffff9dbea41689a0b4b30
Shujauddin94 <shujauddin9432@gmail.com>
2026-05-20 23:20:53 +0500

    added js

--- Prime Number/script.js
+++ Prime Number/script.js
@@ -1,6 +1,6 @@
 // Write a function to check if a given number is prime
 function isPrime(num) {
-    if (num <= 1) return false;
+    if (num <= 1) return false; // 0 and 1 are not prime
     if (num <= 3) return true;
     if (num % 2 === 0 || num % 3 === 0) return false; // divisible by 2 or 3 → not prime
     for (let i = 5; i * i <= num; i += 6) {
