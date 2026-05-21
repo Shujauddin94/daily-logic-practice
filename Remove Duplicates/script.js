fd227cda406ecf740abe07cc0b9b29bddc8c0967
Shujauddin94 <shujauddin9432@gmail.com>
2026-05-21 23:32:25 +0500

    added js

--- Remove Duplicates/script.js
+++ Remove Duplicates/script.js
@@ -2,4 +2,4 @@
 function removeDuplicates(arr) {
     return [...new Set(arr)]; // Using Set to remove duplicates and spread operator to convert it back to an array
 }
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); 
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
