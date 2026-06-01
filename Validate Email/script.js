d9640c33ba638db8278de46cca88c50ec9253532
Shujauddin94 <shujauddin9432@gmail.com>
2026-06-01 22:29:20 +0500

    added js

--- Validate Email/script.js
+++ Validate Email/script.js
@@ -13,4 +13,4 @@ function isValidEmail2(email) { // This regex checks for a more specific pattern
   return regex.test(email); // This regex checks for a more specific pattern of email addresses
 }
 console.log(isValidEmail2("test@example.com"));// true
-console.log(isValidEmail2("invalid-email"));
+console.log(isValidEmail2("invalid-email")); // false
